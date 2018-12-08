f72bd2f1bbe5886a367ec1dc69fb53d4a3bbc9fc
Caio Guimarães <caioguimaraes@MacBook-Pro.local>
2018-12-08 11:23:42 -0200

    Clean up code, install packages and prepare environment

---
+++ src/store/configureStore.dev.js
@@ -0,0 +1,30 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import { browserHistory } from "react-router";
+import { routerMiddleware } from "react-router-redux";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
+import promiseMiddleware from "redux-promise-middleware";
+import logger from "redux-logger";
+import rootReducer from "../reducers";
+import errorMiddleware from "../middleware/error";
+
+const routingMiddleware = routerMiddleware(browserHistory);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default function configureStore(initialState) {
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(
+      applyMiddleware(
+        errorMiddleware,
+        promiseMiddleware(),
+        thunk,
+        routingMiddleware,
+        reduxImmutableStateInvariant(),
+        logger
+      )
+    )
+  );
+}
---
+++ src/store/configureStore.prod.js
@@ -0,0 +1,22 @@
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "../reducers";
+import { browserHistory } from "react-router";
+import { routerMiddleware } from "react-router-redux";
+import thunk from "redux-thunk";
+import promiseMiddleware from "redux-promise-middleware";
+import errorMiddleware from "../middleware/error";
+
+const routingMiddleware = routerMiddleware(browserHistory);
+
+export default function configureStore(initialState) {
+  return createStore(
+    rootReducer,
+    initialState,    
+    applyMiddleware(
+      errorMiddleware,
+      promiseMiddleware(),
+      thunk,
+      routingMiddleware
+    )    
+  );
+}
